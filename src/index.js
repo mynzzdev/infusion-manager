58972a8f3086cd6e1ae32ae804ddd83c6da81bcb
MynzzDev <dev.mynzz@gmail.com>
2025-01-31 19:20:53 -0600

    modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-const {  } = require('discord.js')
+const { Client, Events, GatewayIntentBits} = require('discord.js');
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, readyClient => {
+	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
 
 client.login(process.env.TOKEN)
